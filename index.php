795ab5da95b141eb26f72633de467156328cb14b
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-24 14:04:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css' />
+    <title>Boolshop</title>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>Boolshop</h1>
+        </header>
+    </div>
+
+</body>
+
+</html>
